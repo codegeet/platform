3c3ca88f866bfcdc7ee34e32c8dc7049a1ed002f
Vladimir Prudnikov <vladimir.prudnikov@mynd.co>
2023-10-13 23:34:39 -0700

    initial commit, default spring project

---
+++ src/main/kotlin/io/codegeet/sandbox/SandboxApplication.kt
@@ -0,0 +1,11 @@
+package io.codegeet.sandbox
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SandboxApplication
+
+fun main(args: Array<String>) {
+	runApplication<SandboxApplication>(*args)
+}
---
+++ src/test/kotlin/io/codegeet/sandbox/SandboxApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.codegeet.sandbox
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SandboxApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
