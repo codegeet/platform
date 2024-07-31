9cfc2de4d3c88c3a81c3d57893f84ef0e2683329
Vladimir Prudnikov <vladimir.prudnikov@mynd.co>
2024-07-31 11:18:56 -0700

    Removed the Application class and switched to a procedural approach ;)

--- coderunner/src/main/kotlin/io/codegeet/platform/coderunner/Application.kt
+++ coderunner/src/main/kotlin/io/codegeet/platform/coderunner/Application.kt
@@ -10,48 +10,44 @@ import io.codegeet.platform.common.ExecutionRequest
 import io.codegeet.platform.common.ExecutionResult
 import io.codegeet.platform.common.ExecutionStatus
 
-class Application(private val runner: Runner) {
-
-    private val objectMapper: ObjectMapper = jacksonObjectMapper().apply {
-        propertyNamingStrategy = PropertyNamingStrategies.SNAKE_CASE
-        setSerializationInclusion(JsonInclude.Include.NON_NULL)
-        enable(SerializationFeature.INDENT_OUTPUT)
-    }
-
-    fun run(args: Array<String>) {
-        val result = try {
-            parseInput()?.let { runner.run(it) }
-                ?: ExecutionResult(
-                    status = ExecutionStatus.INTERNAL_ERROR,
-                    error = "Coderunner input is empty."
-                )
-        } catch (e: JsonMappingException) {
-            ExecutionResult(
-                status = ExecutionStatus.INTERNAL_ERROR,
-                error = "Failed to parse coderunner input: ${e.message}"
-            )
-        }
-        println(result.toJson())
-    }
-
-    private fun parseInput(): ExecutionRequest? {
-        val stringBuilder = StringBuilder()
-        while (true) {
-            val line = readlnOrNull()
-            if (line.isNullOrEmpty())
-                break
-            stringBuilder.append(line).append("\n")
-        }
-
-        return stringBuilder.toString()
-            .takeIf { it.isNotEmpty() }
-            ?.let { objectMapper.readValue(it, ExecutionRequest::class.java) }
-    }
-
-    private fun ExecutionResult.toJson(): String =
-        objectMapper.writeValueAsString(this)
+private val objectMapper: ObjectMapper = jacksonObjectMapper().apply {
+    propertyNamingStrategy = PropertyNamingStrategies.SNAKE_CASE
+    setSerializationInclusion(JsonInclude.Include.NON_NULL)
+    enable(SerializationFeature.INDENT_OUTPUT)
 }
 
 fun main(args: Array<String>) {
-    Application(Runner(ProcessExecutor(stats = ProcessStats))).run(args)
+    val runner = Runner(ProcessExecutor(stats = ProcessStats))
+
+    val result = try {
+        parseInput()?.let { runner.run(it) }
+            ?: ExecutionResult(
+                status = ExecutionStatus.INTERNAL_ERROR,
+                error = "Coderunner input is empty."
+            )
+    } catch (e: JsonMappingException) {
+        ExecutionResult(
+            status = ExecutionStatus.INTERNAL_ERROR,
+            error = "Failed to parse coderunner input: ${e.message}"
+        )
+    }
+
+    println(result.toJson())
 }
+
+private fun parseInput(): ExecutionRequest? {
+    val stringBuilder = StringBuilder()
+    while (true) {
+        val line = readlnOrNull()
+        if (line.isNullOrEmpty())
+            break
+        stringBuilder.append(line).append("\n")
+    }
+
+    return stringBuilder.toString()
+        .takeIf { it.isNotEmpty() }
+        ?.let { objectMapper.readValue(it, ExecutionRequest::class.java) }
+}
+
+private fun ExecutionResult.toJson(): String =
+    objectMapper.writeValueAsString(this)
